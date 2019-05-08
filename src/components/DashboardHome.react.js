882dfe2dbee11325a02f243ab08d2a082de96a6f
Dan Esparza <esparza.dan@gmail.com>
2019-05-08 13:58:25 -0400

    Update with basic layout

--- src/components/DashboardHome.react.js
+++ src/components/DashboardHome.react.js
@@ -1,21 +1,40 @@
 //  React
 import React, { Component } from 'react';
 
-import iCalUtils from './../utils/iCalUtils';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  dashgroup: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
 
 class DashboardHome extends Component {  
 
   render() {
 
-    let events = iCalUtils.GetEventsForToday();
-    console.log(events);
+    const { classes } = this.props;
 
     return (
-        <div>
-            The home 
-        </div>
+      <div className={classes.root}>
+        <Grid container spacing={0}>
+          <Grid item xs={6}>
+            <div className={classes.dashgroup}>Time / Weather</div>
+          </Grid>
+          <Grid item xs={6}>
+            <div className={classes.dashgroup}>Date / Calendar</div>
+          </Grid>          
+        </Grid>
+      </div>
     );
   }
 }
 
-export default DashboardHome;
+export default withStyles(styles)(DashboardHome);
